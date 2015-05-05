9fe5eb0d387924d79cd496115fd779eeac67e728
Muzietto <marco_faustinelli@yahoo.com>
2015-05-05 13:59:48 +0200

    starting to introduce khepri

---
+++ kep/spec-runner.js
@@ -0,0 +1,22 @@
+
+require.config({
+  'paths': {
+    'mocha': '../js/lib/mocha',
+    'chai': '../js/lib/chai',
+    'sinon': '../js/lib/sinon-1.11.1'
+  },
+  shim: {
+    'mocha': {
+        init: function () {
+            this.mocha.setup('bdd');
+            return this.mocha;
+        }
+    }
+  }
+});
+
+define(['chai', 'mocha'], (function(chai, mocha) {
+    require(['./test_lambda'], function(require) {
+      mocha.run();
+    });
+}));
