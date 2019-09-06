43ec9cba226c700ff9bf2272a8efc91d248b0ba1
Marco Faustinelli (Muzietto) <marco_faustinelli@yahoo.com>
2019-09-06 08:21:23 +0200

    List instead of array (bad idea...)

---
+++ src/lib/list.js
@@ -0,0 +1,36 @@
+export default List;
+
+// -- Aliases ----------------------------------------------------------
+const clone = Object.create;
+
+// eslint-disable-next-line no-unused-vars
+function unimplemented() {
+  throw new Error('Not implemented.');
+}
+
+// eslint-disable-next-line no-unused-vars
+function noop() {
+  return this;
+}
+
+function List() {
+}
+
+List.prototype = {};
+
+List.of = function(arra) {
+  const result = [...arra];
+  result.prototype = List.prototype;
+  return result;
+};
+
+List.prototype.map = function(fab) {
+  return x => Array.prototype.map.call(x, fab);
+};
+
+List.prototype.fmap = List.prototype.map;
+
+List.prototype.pure = List.of;
+
+// [a -> b] -> [a] -> [b]
+// List.prototype.ap = mfab => fa => null;
